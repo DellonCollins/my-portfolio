38b52ec22269a13b4186cab606c45754ee0db5d4
DellonCollins <collins.dellon@gmail.com>
2023-10-27 12:32:22 -0400

    created a single custom hooks file and a pausable set timeout for the canvas to keep it in sync when the page is hidden since the canvas pauses when hidden

---
+++ src/Util/CustomHooks.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+
+export function useDimensions(container, isRef = true, getter = (e) => [e.clientWidth, e.clientHeight]){
+    const [dimensions, setDimensions] = useState({
+        width: undefined,
+        height: undefined
+    })
+
+    useEffect(() => {
+        function handleResize() {
+            let element = isRef ? container.current : container
+            let [width, height] = getter(element)
+            setDimensions({width: width, height: height})
+        }
+        
+        // Attach the event listener to the window object and initialize
+        window.addEventListener('resize', handleResize)
+        handleResize()
+
+        // Remove the event listener on cleanup
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        };
+    }, []);
+
+    return dimensions
+}
+
+
+
+export function useVisibility() {
+    const [visibility, setVisibility] = useState()
+
+    useEffect(()=> {
+        const visibilityListener = (event) => { 
+            console.log("visiblity", document.visibilityState)
+            setVisibility(document.hidden)
+        }
+
+        window.addEventListener("visibilitychange", visibilityListener,);
+        return () => {
+            window.removeEventListener("visibilitychange", visibilityListener)
+        }
+    }, [])
+
+    return visibility
+}
+
+export function useReload() {
+    const [reloadTriggered, setReloadTriggered] = useState(false)
+
+    useEffect(()=> {
+        const reloadListener = (event) => { 
+            console.log("reload")
+            setReloadTriggered(true)
+        }
+
+        window.addEventListener("beforeunload", reloadListener,);
+        return () => {
+            window.removeEventListener("beforeunload", reloadListener)
+        }
+    }, [])
+
+    return reloadTriggered
+}
---
+++ src/Util/TimerWithPause.js
@@ -0,0 +1,18 @@
+export default function TimerWithPause(callback, delay) {
+    var timerId, start, remaining = delay;
+
+    this.pause = function() {
+        clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function() {
+        start = new Date();
+        clearTimeout(timerId);
+        timerId = setTimeout(callback, remaining);
+    };
+
+    
+
+    this.resume();
+}
