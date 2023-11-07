91c365a6f3ee1f3cf3744e425e48285ad762324c
DellonCollins <collins.dellon@gmail.com>
2023-11-07 11:01:06 -0500

    contacts are rendered from a list

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,16 +4,27 @@ import { CSSTransitionWrapper } from "../../Styles and Transitions/Transitions";
 import PageLayout from "../Layouts/PageLayout";
 import PageTitle from "../PageTitle";
 
+const contacts = [
+    { name : "Email", value : "collins.dellon@gmail.com" },
+    { name : "Github", value : "https://github.com/DellonCollins" },
+]
+
+const renderContacts = () => {
+    return contacts.map((contact, index) => {
+        return <Row className="pt-5 px-2 px-md-5" key={index}>
+        <p className="font-peg heading">{contact.name}</p>
+        <p className="ps-5 default-text fw-light text-wrap text-break">{contact.value}</p>
+    </Row>
+    })
+}
+
 export default function Contact(){
     return <PageLayout>
         <CSSTransitionWrapper classNames="fade">
             <Row className="pt-5">
                 <PageTitle title="Contact"/>
             </Row>
-            <Row className="pt-5 px-2 px-md-5">
-                <p className="font-peg heading">Email</p>
-                <p className="ps-5 default-text fw-light">collins.dellon@gmail.com</p>
-            </Row>
+            {renderContacts()}
         </CSSTransitionWrapper>
     </PageLayout>
 }
