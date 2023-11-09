1b7c070f70eaddee3afdb9efe231a37f830bcffa
DellonCollins <collins.dellon@gmail.com>
2023-11-09 10:45:33 -0500

    changed /home link title to Home

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,19 +1,20 @@
 import { Collapse } from "bootstrap";
 import { useCallback, useRef, } from "react";
 import { Container, Nav, Navbar,  } from "react-bootstrap";
-import { useLocation, useNavigate, useNavigation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import NavigationLink from "./NavigationLink";
 
 const links = [
-    { address : "home", name : "About Me" },
+    { address : "home", name : "Home" },
     { address : "skills", name : "Skills" },
     { address : "contact", name : "Contact" },
     { address : "site", name : "Site" },
+    { address : "edit", name : "Modify" },
+
 ]
 
 export default function NavigationBar(){
     const location = useLocation()
-    
     const collapseRef = useRef()
     const bootstrapCollapse = useCallback(()=>{
         return collapseRef.current ? Collapse.getOrCreateInstance(collapseRef.current) : undefined
@@ -28,7 +29,7 @@ export default function NavigationBar(){
         if(!bootstrapCollapse()) { return }
         bootstrapCollapse().toggle()
     }
-    
+
     const renderNavigationLinks = () => {
         return links.map((link) => (<NavigationLink className={"mx-0 mx-md-3 mx-lg-0 " + (location.pathname.includes(link.address) ? "fw-bold" : "")} to={link.address} key={link.address}>
             <span className="nav-link">{link.name}</span>
@@ -39,7 +40,7 @@ export default function NavigationBar(){
         <Container fluid className="h-100 px-3 px-md-5 px-lg-2">
 
             <NavigationLink to={"/home"}>
-                <Navbar.Brand className="mx-lg-2">D. Collins</Navbar.Brand>
+                <Navbar.Brand className="mx-lg-2 font-peg">D. Collins</Navbar.Brand>
             </NavigationLink>
             
             <button className="navbar-toggler" type="button" onBlur={close} onClick={toggle} 
