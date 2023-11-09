f5be82cef783656a23ae4644d85dd67b5e1ec3ce
DellonCollins <collins.dellon@gmail.com>
2023-11-09 12:08:03 -0500

    added data for icons w/ alt text

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,29 +1,44 @@
 import { Row, Col } from "react-bootstrap";
+import IconWrapper from "../../Assets/Icons/IconWrapper";
 import { CSSTransitionWrapper } from "../../Styles and Transitions/Transitions";
 import PageLayout from "../Layouts/PageLayout";
 import PageTitle from "../PageTitle";
 import "./Skills.scss"
 
+const skillList = [
+    {
+        title : "Front End",
+        items : [ 
+            { text : "HTML / CSS / Sass", icon : "html 5.png", alt : "HTML 5 Icon" },  
+            { text : "Node", icon : "node-js.png", alt : "Node JS Icon"  }, 
+            { text : "React", icon : "react.svg", alt : "React JS Icon"  }, 
+            { text : "Angular", icon : "angular.svg", alt : "Angular Icon"  }]
+    },
+    {
+        title : "Back End",
+        items : [ 
+            { text : "MongoDB", icon : "mongodb.svg", alt : "MongoDB Icon"  }, 
+            { text : "Express", icon : "express-js.png", alt : "Express Icon"  }, 
+            { text : "SQL", icon : "sql.svg", alt : "SQL Icon"  } ]
+    },
+    {
+        title : "Programming Languages",
+        items : [ 
+            { text : "Javascript/Typescript", icon : "javascript.svg", alt : "Javascript Icon" }, 
+            { text : "Java", icon : "java.svg", alt : "Java Icon"  }, 
+            { text : "Python", icon : "python.svg", alt : "PythonPython Icon"  }, 
+            { text : "C#", icon : "c-sharp.svg", alt : "C# Icon"  } ]
+    },
+    {
+        title : "IT Experience",
+        items : [ 
+            { text : "ServiceNow", icon : "servicenow.svg", alt : "ServiceNow Icon"  }, 
+            { text : "SAP", icon : "sap.svg", alt : "SAP Icon"  } 
+        ]
+    }
+]
+
 export default function Skills(){
-    const skillList = [
-        {
-            title : "Front End",
-            items : [ "HTML / CSS / Sass",  "Node", "React", "Angular"]
-        },
-        {
-            title : "Back End",
-            items : [ "MongoDB", "Express", "SQL" ]
-        },
-        {
-            title : "Programming Languages",
-            items : [ "Javascript/Typescript", "Java", "Python", "C#" ]
-        },
-        {
-            title : "IT Experience",
-            items : [ "ServiceNow", "SAP" ]
-        }
-    ]
-    
     return (<PageLayout>
         <CSSTransitionWrapper classNames="fade">
             <Row className="pt-5">
@@ -32,9 +47,12 @@ export default function Skills(){
             <Row className="pt-5 px-5">
                 { skillList.map(skill => { return <Col className="skill-column" md="6" key={skill.title}> 
                     <p className="font-peg heading">{skill.title}</p>
-                    <div className="ps-5 py-2 default-text fw-light">
-                        {skill.items.map((item, itemIndex) => <CSSTransitionWrapper classNames="fade" key={item}>
-                            <p style={{transitionDelay:`${itemIndex*300}ms`}}>{item}</p>
+                    <div className="ps-2 ps-md-5 py-2 default-text fw-light">
+                        {skill.items.map((item, itemIndex) => <CSSTransitionWrapper classNames="fade" key={item + " " + itemIndex}>
+                            <p style={{transitionDelay:`${itemIndex*300}ms`}}>
+                                <IconWrapper resource={item.icon} height="42px" alt={item.alt}/>&nbsp;
+                                {item.text}
+                            </p>
                         </CSSTransitionWrapper>)}
                     </div>
                 </Col>}) }
