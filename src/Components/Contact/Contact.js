7a2ccd2640aaf7618522398a3929e4597289754d
DellonCollins <collins.dellon@gmail.com>
2023-11-09 12:40:35 -0500

    added anchor to links

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,7 +5,7 @@ import PageLayout from "../Layouts/PageLayout";
 import PageTitle from "../PageTitle";
 
 const contacts = [
-    { name : "Email", value : "collins.dellon@gmail.com" },
+    { name : "Email", value : "collins.dellon@gmail.com", email: true },
     { name : "Github", value : "https://github.com/DellonCollins" },
 ]
 
@@ -13,7 +13,11 @@ const renderContacts = () => {
     return contacts.map((contact, index) => {
         return <Row className="pt-5 px-2 px-md-5" key={index}>
         <p className="font-peg heading">{contact.name}</p>
-        <p className="ps-5 default-text fw-light text-wrap text-break">{contact.value}</p>
+        <p className="ps-5 default-text fw-light text-wrap text-break">
+            <a className="link-light link-underline-opacity-50 link-underline-opacity-100-hover" href={ contact.email ? "mailto:" + contact.value : contact.value } target="_blank">
+                {contact.value}
+            </a>
+        </p>
     </Row>
     })
 }
