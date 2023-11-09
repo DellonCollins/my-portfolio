1f6a201a1272750e75cb9cd687507944acc83b53
DellonCollins <collins.dellon@gmail.com>
2023-11-09 13:39:19 -0500

    Page title changes with navigation

--- src/Components/Router/AppRouter.js
+++ src/Components/Router/AppRouter.js
@@ -1,11 +1,18 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
 import Contact from "../Contact/Contact"
 import Home from "../Home/Home"
 import Site from "../Site/Site"
 import Skills from "../Skills/Skills"
 import Edit from "../Edit/Edit"
+import { useEffect } from "react"
 
 export default function AppRouter(){
+    const location = useLocation()
+    useEffect(()=> {
+        let l = location.pathname.substring(1)
+        document.getElementsByTagName("title")[0].innerText = `${l} | .dc ( )`
+    }, [location])
+    
     return (<Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/skills" element={<Skills/>}/>
