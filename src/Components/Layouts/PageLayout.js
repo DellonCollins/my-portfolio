5c41ccb37875fe9b1861b4241330d0ac0e9bef46
DellonCollins <collins.dellon@gmail.com>
2023-11-09 10:44:39 -0500

    nav menu z index set to draw over form controls

--- src/Components/Layouts/PageLayout.js
+++ src/Components/Layouts/PageLayout.js
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 
 export default function PageLayout({children}) {
-    return (<Container fluid="lg" className="page-layout h-100 d-flex flex-column">        
+    return (<Container fluid className="page-layout h-100 d-flex flex-column">        
         {children}
     </Container>)
 }
