fd239a2132c71b41e1484ac239137b38ceedd0e1
DellonCollins <collins.dellon@gmail.com>
2023-11-10 11:15:31 -0500

    clean word breaks for links

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,22 +5,33 @@ import PageLayout from "../Layouts/PageLayout";
 import PageTitle from "../PageTitle";
 
 const contacts = [
-    { name : "Email", value : "collins.dellon@gmail.com", email: true },
-    { name : "Github", value : "https://github.com/DellonCollins" },
+    { 
+        name : "Email", link : "collins.dellon@gmail.com", 
+        text : <>collins<wbr/>.dellon<wbr/>@gmail<wbr/>.com</>, email: true 
+    },
+    { 
+        name : "Github", link : "https://github.com/DellonCollins", 
+        text : <>https://<wbr/>github.<wbr/>com/<wbr/>Dellon<wbr/>Collins</> 
+    }
 ]
 
-const renderContacts = () => {
-    return contacts.map((contact, index) => {
-        return <Row className="pt-5 px-2 px-md-5" key={index}>
-        <p className="font-peg heading">{contact.name}</p>
-        <p className="ps-5 default-text fw-light text-wrap text-break">
-            <a className="link-light link-underline-opacity-50 link-underline-opacity-100-hover" href={ contact.email ? "mailto:" + contact.value : contact.value } target="_blank">
-                {contact.value}
-            </a>
-        </p>
-    </Row>
+const renderContacts = () => (
+    contacts.map((contact, index) => {
+        return <Row className="pt-5 px-2 px-md-5"  key={index}>
+            <CSSTransitionWrapper classNames="fade" key={index}>
+            <div style={{transitionDelay: `${index * 600}ms`}}>
+                <p className="font-peg heading" >{contact.name}</p>
+                <p className="ps-5 default-text fw-light text-wrap text-break">
+                    <a className="link-light link-underline-opacity-50 link-underline-opacity-100-hover" href={ contact.email ? "mailto:" + contact.link : contact.link } 
+                        target="_blank" rel="noreferrer">
+                        {contact.text}
+                    </a>
+                </p>
+            </div>
+            </CSSTransitionWrapper>
+        </Row>
     })
-}
+)
 
 export default function Contact(){
     return <PageLayout>
