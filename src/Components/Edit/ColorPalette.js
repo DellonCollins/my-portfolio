827980777085f36ee2cafe819032f39226c3bfbe
DellonCollins <collins.dellon@gmail.com>
2023-11-10 11:42:09 -0500

    added screen reader accessibility

--- src/Components/Edit/ColorPalette.js
+++ src/Components/Edit/ColorPalette.js
@@ -31,23 +31,23 @@ export default function ColorPalette(){
 
     const colorMap = (value, index) => {
         return <Col className="mb-1" md={12} key={index}>
-            <InputGroup key={index}>
-                <InputGroupText style={{fontWeight: 600, letterSpacing:"1px"}}>Color {index + 1}</InputGroupText>
+            <InputGroup key={index} role="group" title={`Color Group ${index + 1}`}>
+                <InputGroupText style={{fontWeight: 600, letterSpacing:"1px"}}> {`Color ${index + 1}`}</InputGroupText>
 
-                <Form.Control className="h-auto" type="color" value={value} onChange={changeColor} id={`${index}`} title={`Color ${index + 1}`}></Form.Control>
+                <Form.Control className="h-auto" type="color" value={value} onChange={changeColor} id={`${index}`} title={`Select Color ${index + 1}`}></Form.Control>
                 
-                <Button className="p-2" variant="primary" onClick={addColor} data-index={`${index}`} title="add color">
-                    <i className="bi bi-plus-lg" style={{fontSize:"1.5rem"}} data-index={`${index}`} alt="add color"/>
+                <Button className="p-2" variant="primary" onClick={addColor} data-index={`${index}`} aria-label="add color">
+                    <i className="bi bi-plus-lg" style={{fontSize:"1.5rem"}} data-index={`${index}`} alt="add color" aria-hidden="true"/>
                 </Button>
 
-                <Button className="p-2" variant={colors.length <= minNumColors ? "outline-danger" : "danger"} onClick={removeColor} data-index={`${index}`} title="remove color" disabled={colors.length <= minNumColors}>
-                    <i className="bi bi-dash-lg" style={{fontSize:"1.5rem"}} data-index={`${index}`} alt="remove color"/>
+                <Button className="p-2" variant={colors.length <= minNumColors ? "outline-danger" : "danger"} onClick={removeColor} data-index={`${index}`} aria-label="remove color" disabled={colors.length <= minNumColors}>
+                    <i className="bi bi-dash-lg" style={{fontSize:"1.5rem"}} data-index={`${index}`} alt="remove color" aria-hidden="true"/>
                 </Button>
             </InputGroup>
         </Col>
     }
     return <>
-    <Row>
+    <Row aria-hidden>
         <div><i className="bi bi-paint-bucket" style={{fontSize:"3rem"}} alt="palette icon"/></div>
     </Row>
     {colors.map(colorMap)}
