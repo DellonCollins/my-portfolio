b315276eacf8c2d95c275c139fd18922a727cfef
DellonCollins <collins.dellon@gmail.com>
2023-11-13 11:31:34 -0500

    add icons related to contact methods

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,11 +7,13 @@ import PageTitle from "../PageTitle";
 const contacts = [
     { 
         name : "Email", link : "collins.dellon@gmail.com", 
-        text : <>collins<wbr/>.dellon<wbr/>@gmail<wbr/>.com</>, email: true 
+        text : <>collins<wbr/>.dellon<wbr/>@gmail.com</>, email: true,
+        icon : <i className="bi bi-envelope" style={{fontSize:"2rem"}} alt="envelope icon" aria-hidden="true"/>
     },
     { 
         name : "Github", link : "https://github.com/DellonCollins", 
-        text : <>https://<wbr/>github.<wbr/>com/<wbr/>Dellon<wbr/>Collins</> 
+        text : <>https://<wbr/>github.<wbr/>com/<wbr/>Dellon<wbr/>Collins</>,
+        icon : <i className="bi bi-github" style={{fontSize:"2rem"}} alt="gitub icon" aria-hidden="true"/>
     }
 ]
 
@@ -21,8 +23,9 @@ const renderContacts = () => (
             <CSSTransitionWrapper classNames="fade" key={index}>
             <div style={{transitionDelay: `${index * 600}ms`}}>
                 <p className="font-peg heading" >{contact.name}</p>
-                <p className="ps-5 default-text fw-light text-wrap text-break">
-                    <a className="link-light link-underline-opacity-50 link-underline-opacity-100-hover" href={ contact.email ? "mailto:" + contact.link : contact.link } 
+                <p className="d-flex ps-5 default-text fw-light text-wrap text-break">
+                    {contact.icon}
+                    <a className="ms-2 link-light link-underline-opacity-50 link-underline-opacity-100-hover my-auto" href={ contact.email ? "mailto:" + contact.link : contact.link } 
                         target="_blank" rel="noreferrer">
                         {contact.text}
                     </a>
