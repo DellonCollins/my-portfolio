1654d961884c904126f518a06156ab994a50019d
DellonCollins <collins.dellon@gmail.com>
2023-11-14 13:41:14 -0500

    reorganized import order

--- src/Components/Router/AppRouter.js
+++ src/Components/Router/AppRouter.js
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom"
+import { useEffect } from "react"
 import Contact from "../Contact/Contact"
 import Home from "../Home/Home"
 import Site from "../Site/Site"
 import Skills from "../Skills/Skills"
 import Edit from "../Edit/Edit"
-import { useEffect } from "react"
+
 
 export default function AppRouter(){
     const location = useLocation()
