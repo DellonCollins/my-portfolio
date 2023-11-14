f1cd01a278c3397bc6d2cc8e57236924f42f58df
DellonCollins <collins.dellon@gmail.com>
2023-11-14 14:42:36 -0500

    node packages are now links rendered dynamically

--- src/Components/Site/Site.js
+++ src/Components/Site/Site.js
@@ -3,6 +3,14 @@ import { CSSTransitionWrapper } from "../../Styles and Transitions/Transitions";
 import PageLayout from "../Layouts/PageLayout";
 import PageTitle from "../PageTitle";
 
+const packages = [
+    { text : "React", color : "#61DBFB", link : "https://react.dev"},
+    { text : "Zustand", color : "#DDDDDD", link : "https://github.com/pmndrs/zustand"},
+    { text : "Bootstrap", color : "#712cf9", link : "https://react-bootstrap.netlify.app"},
+    { text : "Sass", color : "#CD6799", link : "https://sass-lang.com"},
+    { text : "P5js", color : "#ed225d", link : "https://p5js.org"},
+]
+
 export default function Site(){
     return <PageLayout>
     <CSSTransitionWrapper classNames="fade">
@@ -12,19 +20,28 @@ export default function Site(){
         <Row className="pt-5 px-md-5">
             <p className="font-peg heading"></p>
             <p className="ps-md-5 default-text fw-light">
-                This site was made using <span className="fw-normal" style={{color:"#61DBFB"}}>React</span>,
-                <span className="fw-normal" style={{color:"#712cf9"}}> Bootstrap</span>,
-                <span className="fw-normal" style={{color:"#CD6799"}}> Sass</span>, and 
-                <span className="fw-normal" style={{color:"#ed225d"}}> P5js</span> </p>
+                <span>This site was made using </span>
+                { packages.map((pkg, index) => { return <>
+                    <a className="fw-normal link-opacity-25 link-opacity-100-hover" key={index} href={pkg.link} target="_blank" rel="noreferrer" style={{color : pkg.color}}>
+                        {pkg.text}
+                    </a>
+                    { index < packages.length-2 
+                        ?   ", "
+                        :   (index === packages.length-2 
+                                ? ", and "
+                                : ".")
+                    }
+                </>})}
+            </p>
         </Row>
-        <Row className="px-md-5">
+        <Row className="px-md-5 pb-3">
             <p className="font-peg heading">How the canvas works</p>
             <div className="ps-md-5 default-text fw-light">
-                <p> The background is generative art inspired by flow fields. </p>
-                <p> Under the screen is a grid of vectors. Perlin noise is used to generate the direction of the grid points. Perlin noise commonly used because it generates
+                <p> The background is generative art inspired by flow fields. P5js is the package that controls the render.</p>
+                <p> Under the screen is a grid of vectors. <a className="link-light" href="https://en.wikipedia.org/wiki/Perlin_noise" target="_blank" rel="noreferrer">Perlin noise</a> is used to generate the direction of the grid points. Perlin noise commonly used because it generates
                     random values that are correlated/influenced by nearby values. The lines drawn on the screen trace the path of particles as they 
-                    are influenced by the grid. This results in smooth curves that tend to flow in one direction. The visual affect of this canvas is smiliar to flowing
-                    strands of hair.
+                    are influenced by the grid. This results in smooth curves that tend to flow in one direction. The visual affect of this canvas is smiliar to flowing rivers,
+                    magnetic fields, or strands of hair.
                 </p>
             </div>
         </Row>
