56793428be511c4f0645281421df8ffd540acc8d
DellonCollins <collins.dellon@gmail.com>
2023-11-16 20:36:31 -0500

    reorganized to group the store by data type

--- src/Components/Edit/EditCanvas.js
+++ src/Components/Edit/EditCanvas.js
@@ -8,49 +8,58 @@ export default function EditCanvas({ref}){
     const [reset, setReset] = useState(false)
     const inputRefs = useRef({})
 
-    const drawDuration = useCanvasStore((state)=> state.drawDuration)
-    const gridDensity = useCanvasStore((state)=> state.gridDensity)
-    const particleDensity = useCanvasStore((state)=> state.particleDensity)
-    const chaos = useCanvasStore((state)=> state.chaos)
+    const parameters = useCanvasStore(state => ({
+        drawDuration : state.drawDuration,
+        gridDensity : state.gridDensity,
+        particleDensity : state.particleDensity,
+        chaos : state.chaos,
+        playSwitch : state.playSwitch,
+    }))
 
-    const setDrawDuration = useCanvasStore((state)=> state.setDrawDuration)
-    const setGridDensity = useCanvasStore((state)=> state.setGridDensity)
-    const setParticleDensity = useCanvasStore((state)=> state.setParticleDensity)
-    const setChaos = useCanvasStore((state)=> state.setChaos)
+    const set = useCanvasStore(state => ({
+        drawDuration : state.setDrawDuration,
+        gridDensity : state.setGridDensity,
+        particleDensity : state.setParticleDensity,
+        chaos : state.setChaos
+    }))
 
-    const toggleSaveSwitch = useCanvasStore((state)=> state.toggleSaveSwitch)
-    const toggleResetSwitch = useCanvasStore((state)=>state.toggleResetSwitch)
+    const toggle = useCanvasStore(state => ({
+        save : state.toggleSaveSwitch,
+        reset : state.toggleResetSwitch,
+        play : state.togglePlaySwitch
+    }))
     
     const handleDrawDuration = (e) => {
         if((e.type === 'keydown' && e.key === 'Enter') || e.type === 'blur'){
-            if(!inputRefs.current.drawDuration.value) { inputRefs.current.drawDuration.value = 10 }
-            setDrawDuration(inputRefs.current.drawDuration.value)
+            if(!inputRefs.current.drawDuration.value || inputRefs.current.drawDuration.value < 10) { inputRefs.current.drawDuration.value = 10}
+            inputRefs.current.drawDuration.blur()
+            set.drawDuration(inputRefs.current.drawDuration.value)
         }
     }
 
     const handleGridDensity = (e)=> {
-        setDrawDuration(inputRefs.current.drawDuration.value)
-        setGridDensity(e.target.value / 10)
+        set.drawDuration(inputRefs.current.drawDuration.value)
+        set.gridDensity(e.target.value / 10)
     }
 
     const handleParticleDensity = (e)=> {
-        setDrawDuration(inputRefs.current.drawDuration.value)
-        setParticleDensity(e.target.value / 10)
+        set.drawDuration(inputRefs.current.drawDuration.value)
+        set.particleDensity(e.target.value / 10)
     }
 
     const handleChaos = (e)=> {
-        setChaos(e.target.value)
+        set.chaos(e.target.value)
     }
 
     const resetSettings = () => {
         inputRefs.current.drawDuration.value = 10
-        setDrawDuration(10)
+        set.drawDuration(10)
         inputRefs.current.gridDensity.value = 30
-        setGridDensity(3)
+        set.gridDensity(3)
         inputRefs.current.particleDensity.value = 30
-        setParticleDensity(3)
+        set.particleDensity(3)
         inputRefs.current.chaos.value = 50
-        setChaos(50)
+        set.chaos(50)
         setReset(true)
     }
 
@@ -58,22 +67,22 @@ export default function EditCanvas({ref}){
         { 
             name : "Draw Duration", 
             icon : "bi bi-alarm",
-            control : <Form.Control className="text-center" ref={(el) => inputRefs.current.drawDuration = el} defaultValue={drawDuration} placeholder="10" type="number" min="10" max="250" onKeyDown={handleDrawDuration} onBlur={handleDrawDuration} />
+            control : <Form.Control className="text-center" ref={(el) => inputRefs.current.drawDuration = el} defaultValue={parameters.drawDuration} placeholder="length in seconds" type="number" min={10} max={250} onKeyDown={handleDrawDuration} onBlur={handleDrawDuration} />
         },
         { 
             name : "Grid Density", 
             icon : "bi bi-grid-3x3",
-            control : <Form.Control ref={(el) => inputRefs.current.gridDensity = el} defaultValue={gridDensity * 10} type="range" min="10" max="100" onChange={handleGridDensity}/>
+            control : <Form.Control ref={(el) => inputRefs.current.gridDensity = el} defaultValue={parameters.gridDensity * 10} type="range" min="10" max="100" onChange={handleGridDensity}/>
         },
         { 
             name : "Particle Quantity", 
             icon : "bi bi-three-dots",
-            control : <Form.Control ref={(el) => inputRefs.current.particleDensity = el} defaultValue={particleDensity * 10} type="range" min="10" max="100" onChange={handleParticleDensity} />
+            control : <Form.Control ref={(el) => inputRefs.current.particleDensity = el} defaultValue={parameters.particleDensity * 10} type="range" min="10" max="100" onChange={handleParticleDensity} />
         },
         { 
             name : "Chaos", 
             icon : "bi bi-dice-5",
-            control : <Form.Control ref={(el) => inputRefs.current.chaos = el} defaultValue={chaos} type="range" min="10" max="90" onChange={handleChaos} />
+            control : <Form.Control ref={(el) => inputRefs.current.chaos = el} defaultValue={parameters.chaos} type="range" min="10" max="90" onChange={handleChaos} />
         }
     ]
     
@@ -98,23 +107,39 @@ export default function EditCanvas({ref}){
                     </InputGroup>
                 </Col>
             }) }
-        </Row>
-            
-        <Row className="pt-3">
-            <Col className="mb-1" md={6} role="none">
-                <button className="btn btn-pink text-white w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" onClick={toggleResetSwitch} title="Clear the canvas">
-                    Clear &nbsp;
-                    <i className="bi bi-eraser" style={{fontSize:"2rem"}} aria-hidden />
-                </button>
-            </Col>
-            <Col className="mb-1" md={6} role="none">
-                <Button className="w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" variant="success" onClick={resetSettings} title="Reset parameters to default">
+            <Col md={6} role="none">
+                <Button className="w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" variant="light" onClick={resetSettings} title="Reset parameters to default">
                     <span className="py-auto">Reset &nbsp;</span>
                     <i className="bi bi-arrow-repeat" style={{fontSize:"2rem"}} aria-hidden />
                 </Button>
             </Col>
+        </Row>
+            
+        <Row className="pt-3">
+            <Col className="mb-1" md={6} role="none">
+                <Button className="w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" variant="primary" onClick={toggle.play} title="Play/Pause the canvas">
+                    {
+                        parameters.playSwitch 
+                            ?   <>
+                                    Pause &nbsp;
+                                    <i className="bi bi-pause" style={{fontSize:"2rem"}} aria-hidden />
+                                </>
+                            :   <>
+                                    Play &nbsp;
+                                    <i className="bi bi-play" style={{fontSize:"2rem"}} aria-hidden />
+                                </>
+                    }
+                </Button>
+            </Col>
+            <Col className="mb-1" md={6} role="none">
+                <Button className="btn btn-pink text-white w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" onClick={toggle.reset} title="Clear the canvas">
+                    Clear &nbsp;
+                    <i className="bi bi-eraser" style={{fontSize:"2rem"}} aria-hidden />
+                </Button>
+            </Col>
+            
             <Col md={6} role="none">
-                <Button className="w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" variant="secondary" onClick={toggleSaveSwitch} title="Save the background as an image">
+                <Button className="w-100 flex-grow-1 fw-bold d-inline-flex align-items-center justify-content-center py-0" variant="secondary" onClick={toggle.save} title="Save the background as an image">
                     <span className="py-auto">Save &nbsp;</span>
                     <i className="bi bi-floppy" style={{fontSize:"2rem"}} aria-hidden />
                 </Button>
