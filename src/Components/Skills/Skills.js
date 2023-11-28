dbbab16eab1ac004bcc5181c6527abd559bd3989
DellonCollins <collins.dellon@gmail.com>
2023-11-28 14:32:26 -0500

    resume download added

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -42,9 +42,20 @@ export default function Skills(){
     return (<PageLayout>
         <CSSTransitionWrapper classNames="fade">
             <Row className="pt-5">
-                <PageTitle title="Skills"/>
+                <Col md={4} className="d-flex justify-content-between align-items-center">
+                    <PageTitle title="Skills"/>
+                </Col>
+                <Col className="d-flex align-items-center">
+                    <a className="ps-3 w-auto d-flex align-items-center default-text link-light link-offset-1
+                        link-opacity-75 link-opacity-100-hover
+                        link-underline-opacity-0 link-underline-opacity-100-hover" 
+                        style={{transition:"text-decoration-color 200ms ease, color 200ms ease", textDecorationThickness:"1px"}}
+                        href="/Dellon Collins Resume.pdf" download title="Download Résumé"> 
+                        <span>Résumé</span><i className="bi bi-file-text ps-2" style={{fontSize:"2rem"}} aria-hidden/>
+                    </a>
+                </Col>
             </Row>
-            <Row className="pt-5 px-5">
+            <Row className="pt-3 px-5">
                 { skillList.map(skill => { return <Col className="skill-column" md="6" key={skill.title}> 
                     <p className="font-peg heading">{skill.title}</p>
                     <div className="ps-2 ps-md-5 py-2 default-text fw-light">
